6bd13081048fbb72c82dddc15a83b8bd223b5f3c
Mark <wushu4@yahoo.com.vn>
2021-10-13 07:53:59 -0400

    Added player firing capability

--- Assets/[Scripts]/BulletManager.cs
+++ Assets/[Scripts]/BulletManager.cs
@@ -6,8 +6,10 @@ using UnityEngine;
 [System.Serializable]
 public class BulletManager : MonoBehaviour
 {
-    public Queue<GameObject> bulletPool;
-    public int bulletNumber;
+    public Queue<GameObject> enemyBulletPool;
+    public Queue<GameObject> playerBulletPool;
+    public int enemyBulletNumber;
+    public int playerBulletNumber;
 
 
     private BulletFactory factory;
@@ -15,27 +17,31 @@ public class BulletManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        bulletPool = new Queue<GameObject>(); // creates an empty Queue
+        enemyBulletPool = new Queue<GameObject>(); // creates an empty Enemy Queue
+        playerBulletPool = new Queue<GameObject>(); // creates an empty Player Queue
         factory = GetComponent<BulletFactory>(); // gets a reference to the Bullet Factory code
 
         //BuildBulletPool();
     }
 
-    /// <summary>
-    ///  This method builds a bullet pool of bulletNumber bullets
-    /// </summary>
-    private void BuildBulletPool()
-    {
-        for (int i = 0; i < bulletNumber; i++)
-        {
-            AddBullet();
-        }
-    }
 
-    private void AddBullet()
+    private void AddBullet(BulletType type = BulletType.ENEMY)
     {
-        var temp_bullet = factory.createBullet();
-        bulletPool.Enqueue(temp_bullet);
+        var temp_bullet = factory.createBullet(type);
+
+        switch (type)
+        {
+            case BulletType.ENEMY:
+                enemyBulletPool.Enqueue(temp_bullet);
+                enemyBulletNumber++;
+                break;
+            case BulletType.PLAYER:
+                playerBulletPool.Enqueue(temp_bullet);
+                playerBulletNumber++;
+                break;
+
+        }
+        
     }
 
     /// <summary>
@@ -44,17 +50,36 @@ public class BulletManager : MonoBehaviour
     /// </summary>
     /// <param name="spawnPosition"></param>
     /// <returns></returns>
-    public GameObject GetBullet(Vector2 spawnPosition)
+    public GameObject GetBullet(Vector2 spawnPosition, BulletType type = BulletType.ENEMY)
     {
-        if (bulletPool.Count < 1)
+        GameObject temp_bullet = null;
+        switch(type)
         {
-            AddBullet();
-            bulletNumber++;
-        }
+            case BulletType.ENEMY:
+                if (enemyBulletPool.Count < 1)
+                {
+                    AddBullet(BulletType.ENEMY);
 
-        var temp_bullet = bulletPool.Dequeue();
-        temp_bullet.transform.position = spawnPosition;
-        temp_bullet.SetActive(true);
+                }
+
+                temp_bullet = enemyBulletPool.Dequeue();
+                temp_bullet.transform.position = spawnPosition;
+                temp_bullet.SetActive(true);
+                
+                break;
+            case BulletType.PLAYER:
+                if (playerBulletPool.Count < 1)
+                {
+                    AddBullet(BulletType.PLAYER);
+
+                }
+
+                temp_bullet = playerBulletPool.Dequeue();
+                temp_bullet.transform.position = spawnPosition;
+                temp_bullet.SetActive(true);
+                
+                break;
+        }
         return temp_bullet;
     }
 
@@ -62,9 +87,18 @@ public class BulletManager : MonoBehaviour
     /// This method returns a bullet back into the bullet pool
     /// </summary>
     /// <param name="returnedBullet"></param>
-    public void ReturnBullet(GameObject returnedBullet)
+    public void ReturnBullet(GameObject returnedBullet, BulletType type = BulletType.ENEMY)
     {
         returnedBullet.SetActive(false);
-        bulletPool.Enqueue(returnedBullet);
+        switch(type)
+        {
+            case BulletType.ENEMY:
+                enemyBulletPool.Enqueue(returnedBullet);
+                break;
+            case BulletType.PLAYER:
+                playerBulletPool.Enqueue(returnedBullet);
+                break;
+        }
+        
     }
 }
